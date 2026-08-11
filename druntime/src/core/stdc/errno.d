8fd6649dd3100c9f4f41d4b301777684ef2f878b
Dennis Korpel <dkorpel@gmail.com>
2026-08-12 00:30:57 +0200

    Remove WASI_Compat

--- druntime/src/core/stdc/errno.d
+++ druntime/src/core/stdc/errno.d
@@ -23,9 +23,6 @@ else version (TVOS)
 else version (WatchOS)
     version = Darwin;
 
-version (WASI)          version = WASI_Compat;
-version (CRuntime_WASI) version = WASI_Compat;
-
 version (ARM)     version = ARM_Any;
 version (AArch64) version = ARM_Any;
 version (HPPA)    version = HPPA_Any;
@@ -162,15 +159,6 @@ else version (Haiku)
     {
         ref int _errnop();
         alias errno = _errnop;
-    }
-}
-else version (WASI_Compat)
-{
-    // wasi-libc exposes errno as a plain global, rt.wasm.extra wraps it
-    extern (C)
-    {
-        ref int __errno_location();
-        alias errno = __errno_location;
     }
 }
 else
